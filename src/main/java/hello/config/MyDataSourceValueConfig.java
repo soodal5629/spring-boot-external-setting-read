9ce6432a683de75383db9e0e425ac0323fcb3601
chaelynjang <chaelynjang@lotte.net>
2023-08-13 20:59:40 +0900

    @Value 사용해보기

---
+++ src/main/java/hello/config/MyDataSourceValueConfig.java
@@ -0,0 +1,49 @@
+package hello.config;
+
+import hello.datasource.MyDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.List;
+
+@Slf4j
+@Configuration
+public class MyDataSourceValueConfig {
+
+    @Value("${my.datasource.url}")
+    private String url;
+    @Value("${my.datasource.username}")
+    private String username;
+    @Value("${my.datasource.password}")
+    private String password;
+    @Value("${my.datasource.etc.max-connection}")
+    private int maxConnection;
+    @Value("${my.datasource.etc.timeout}")
+    private Duration timeout;
+    @Value("${my.datasource.etc.option}")
+    private List<String> options;
+
+    // 외부 설정값 없는 경우 다음과 같이 default 값(no) 사용 가능
+    @Value("${my.notFound:no}")
+    private String notFound;
+    @Bean
+    public MyDataSource myDataSource1() {
+        return new MyDataSource(url, username, password, maxConnection, timeout, options);
+    }
+
+    @Bean
+    // @Value는 파라미터로도 주입 가능
+    public MyDataSource myDataSource2(
+            @Value("${my.datasource.url}") String url,
+            @Value("${my.datasource.username}") String username,
+            @Value("${my.datasource.password}") String password,
+            @Value("${my.datasource.etc.max-connection}") int maxConnection,
+            @Value("${my.datasource.etc.timeout}") Duration timeout,
+            @Value("${my.datasource.etc.option}") List<String> options) {
+        log.info("외부 설정값 없는 경우 default 값 사용 가능: {}", notFound);
+        return new MyDataSource(url, username, password, maxConnection, timeout, options);
+    }
+}
