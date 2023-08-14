d8f82881211403e72f2e25aaab440078edc6c371
chaelynjang <chaelynjang@lotte.net>
2023-08-14 17:56:41 +0900

    @ConfigurationProperties 생성자 이용해서 외부설정 값 읽고 빈 등록

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -1,6 +1,7 @@
 package hello;
 
 import hello.config.MyDataSourceConfigV1;
+import hello.config.MyDataSourceConfigV2;
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceConfigV1.class)
+@Import(MyDataSourceConfigV2.class)
+//@Import(MyDataSourceConfigV1.class)
 //@Import(MyDataSourceValueConfig.class)
 //@Import(MyDataSourceEnvConfig.class)
 @ConfigurationPropertiesScan
---
+++ src/main/java/hello/datasource/MyDataSourcePropertiesV2.java
@@ -0,0 +1,40 @@
+package hello.datasource;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+// 외부설정을 주입받는 객체라는 뜻
+// 여기에 외부 설정 key의 묶음 시작점인 my.datasource 적어줌
+@ConfigurationProperties("my.datasource")
+public class MyDataSourcePropertiesV2 {
+    private String url;
+    private String username;
+    private String password;
+    private Etc etc;
+
+    public MyDataSourcePropertiesV2(String url, String username, String password, @DefaultValue Etc etc) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.etc = etc;
+    }
+
+    @Getter
+    public static class Etc {
+        private int maxConnection;
+        private Duration timeout;
+        private List<String> options;
+
+        public Etc(int maxConnection, Duration timeout, @DefaultValue("DEFAULT") List<String> options) {
+            this.maxConnection = maxConnection;
+            this.timeout = timeout;
+            this.options = options;
+        }
+    }
+}
